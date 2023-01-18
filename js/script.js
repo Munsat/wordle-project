da73dc929f983f593782eb31c4eb6ea0ec2eccac
Munsat Rukaya <munsatrukaya@192-168-1-117.tpgi.com.au>
2023-01-18 23:39:10 +1100

    added keyboard display

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
-for (let i = 0; i < 6; i++) {
-  const lineHolder = document.createElement("div");
-  lineHolder.className = "line-holder";
-  document.querySelector(".display-box").appendChild(lineHolder);
-  for (let i = 0; i < 5; i++) {
-    const wordHolder = document.createElement("div");
-    wordHolder.className = "word-holder";
-    lineHolder.appendChild(wordHolder);
-  }
+const createLetterBoxes = () => {
+    for (let i = 0; i < 6; i++) {
+        const lineHolder = document.createElement("div");
+        lineHolder.classList.add("line-holder", `row${i+1}`) 
+        document.querySelector(".display-box").appendChild(lineHolder);
+        for (let i = 0; i < 5; i++) {
+          const wordHolder = document.createElement("div");
+          wordHolder.className = "word-holder";
+          lineHolder.appendChild(wordHolder);
+        }
+      }
 }
+
+createLetterBoxes()
+
+const createKeyboard = () => {
+    
+}
+
+
+
+
