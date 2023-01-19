6615133a322e1bd1589a5d81188742dc191aca40
Munsat Rukaya <munsatrukaya@192-168-1-117.tpgi.com.au>
2023-01-19 11:18:11 +1100

    added function key functionalities, check for 5 word, toggle-enterkey

--- js/script.js
+++ js/script.js
@@ -12,30 +12,70 @@ const createLetterBoxes = () => {
   }
 };
 createLetterBoxes();
+
 let word = "";
 const allKeys = document.querySelectorAll(".key");
 const row1 = document.querySelectorAll(".row1.wordholder");
 const allLetter = document.querySelectorAll(".line-holder.row1 > .word-holder");
+const enterKey = document.querySelector("#enter");
 
 const displayLetter = () => {
-  for (i = 0; i < word.length; i++) {
+  for (i = 0; i < allLetter.length; i++) {
     allLetter[i].textContent = word[i];
+  }
+};
+
+const checkLetter = () => {
+  if (word.length === 5 && validWords.includes(word)) {
+    enterKey.disabled = false;
+  }else{
+    enterKey.disabled = true;
   }
 };
 
 window.addEventListener("keyup", (e) => {
   const isNumber = isFinite(e.key);
   if (!isNumber) {
-    word += e.key.toUpperCase();
-    console.log(word);
+    switch (e.key) {
+      case "Backspace":
+        word = word.slice(0, -1);
+        break;
+      case "Enter":
+        if (enterKey.disabled) {
+          e.preventDefault();
+          console.log("key NOT entered");
+        } else {
+          console.log("key entered");
+        }
+        break;
+      default:
+        word += e.key.toUpperCase();
+    }
+
     displayLetter();
+    checkLetter();
   }
 });
 
 allKeys.forEach((key) => {
   key.addEventListener("click", (e) => {
-    word += e.target.textContent;
-    console.log(word);
+    switch (e.target.id) {
+      case "backspace":
+        word = word.slice(0, -1);
+        break;
+      case "enter":
+        if (enterKey.disabled) {
+          e.preventDefault();
+          console.log("key NOT entered");
+        } else {
+          console.log("key entered");
+        }
+        break;
+      default:
+        word += e.target.textContent;
+    }
+
     displayLetter();
+    checkLetter();
   });
 });
