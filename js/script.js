f2f6498ffcdd2c502aeacbca68153ad31f272d81
Munsat Rukaya <munsatrukaya@192-168-1-117.tpgi.com.au>
2023-01-21 16:58:28 +1100

    added font, winner display functionality

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-const wordOfTheGame = validWords[Math.floor(Math.random()*validWords.length)]
-console.log(wordOfTheGame)
+const wordOfTheGame = validWords[Math.floor(Math.random() * validWords.length)];
+console.log(wordOfTheGame);
+let word = "";
+let num = 0;
+let lineComplete = false;
+let greenLength = 0;
+const allKeys = document.querySelectorAll(".key");
+const enterKey = document.querySelector("#enter");
+const winnerDisplay = document.querySelector(".winner-display");
+const winnerTexts = ["GOOD JOB!", "WELL DONE", "WAY TO GO!", "WINNER!", "WOW!"];
+
 const createLetterBoxes = () => {
   for (let i = 0; i < 6; i++) {
     const lineHolder = document.createElement("div");
-    lineHolder.classList.add("line-holder", `row${i + 1}`);
+    lineHolder.classList.add("line-holder", `row${i}`);
     document.querySelector(".display-box").appendChild(lineHolder);
     for (let i = 0; i < 5; i++) {
       const wordHolder = document.createElement("div");
@@ -14,19 +23,15 @@ const createLetterBoxes = () => {
 };
 createLetterBoxes();
 
-let word = "";
-const allKeys = document.querySelectorAll(".key");
-const row1 = document.querySelectorAll(".row1.wordholder");
-const allLetter = document.querySelectorAll(".line-holder.row1 > .word-holder");
-const enterKey = document.querySelector("#enter");
-
-const displayLetter = () => {
+//Displays letter for each line
+const displayLetter = (allLetter) => {
   for (i = 0; i < allLetter.length; i++) {
     allLetter[i].textContent = word[i];
   }
 };
 
-const checkLetter = () => {
+//Checks whether the word is part of the vocab
+const checkLetter = (word) => {
   if (word.length === 5 && validWords.includes(word)) {
     enterKey.disabled = false;
   } else {
@@ -34,53 +39,93 @@ const checkLetter = () => {
   }
 };
 
-const displayAnswer = () => {
-  for (i=0;i<word.length;i++){
-    if (wordOfTheGame[i]===word[i]){
-      allLetter[i].style.backgroundColor = 'green'
-    }else if (wordOfTheGame.includes(word[i])){
-      allLetter[i].style.backgroundColor = 'orange'
-    }else{
-      allLetter[i].style.backgroundColor = 'grey'
+//Checks if the user has won the round
+const checkWinner = (allLetter) => {
+  allLetter.forEach((letter) => {
+    if (letter.style.backgroundColor === "rgb(96, 159, 141)") {
+      greenLength++;
+    }
+    if (greenLength === 5) {
+      winnerDisplay.querySelector("h1").textContent =
+        winnerTexts[Math.floor(Math.random() * winnerTexts.length)];
+        winnerDisplay.querySelector('a').textContent = 'next level'
+      winnerDisplay.style.opacity = 1;
+    }
+  });
+  greenLength = 0;
+};
+
+//Checks if the user has lost the round
+const checkGameOver = (num) => {
+  if (num > 5) {
+    winnerDisplay.querySelector("h1").textContent = "GAME OVER";
+    winnerDisplay.querySelector('a').textContent = 'start over'
+    winnerDisplay.style.opacity = 1;
+  }
+};
+
+//Displays the coloured tiles for each answer
+const displayAnswer = (allLetter) => {
+  for (i = 0; i < word.length; i++) {
+    if (wordOfTheGame[i] === word[i]) {
+      allLetter[i].style.backgroundColor = "rgb(96, 159, 141)";
+    } else if (wordOfTheGame.includes(word[i])) {
+      allLetter[i].style.backgroundColor = "rgb(190, 101, 63)";
+    } else {
+      allLetter[i].style.backgroundColor = "grey";
     }
   }
-}
+  lineComplete = true;
+  num++;
+  checkWinner(allLetter);
+  checkGameOver(num)
+};
 
-const keyFunction = (e) => {
+
+const keyFunction = (e, allLetter) => {
   if (e.target.id === "backspace" || e.key === "Backspace") {
     word = word.slice(0, -1);
   } else if (e.target.id === "enter" || e.key === "Enter") {
     if (enterKey.disabled) {
       e.preventDefault();
-      console.log("key NOT entered");
     } else {
-      console.log("key entered");
-      displayAnswer()
+      displayAnswer(allLetter);
     }
-  } else if ((e.key >= "a" && e.key <= "z") || (e.getModifierState('CapsLock') && (e.key >= "A" && e.key <= "Z" && e.key.length ===1))){
+  } else if ((e.key >= "a" && e.key <= "z") || (e.getModifierState("CapsLock") && (e.key >= "A" && e.key <= "Z" ) && e.key.length === 1)){
     word += e.key.toUpperCase();
-  }else if (e.type === "click") {
+  } else if (e.type === "click") {
     word += e.target.textContent;
   }
 };
 
-
-
-window.addEventListener("keyup", (e) => {
-  const isNumber = isFinite(e.key);
-  if (!isNumber) {
-    keyFunction(e);
-    checkLetter();
-    displayLetter();
-    
+const repeatFunction = (e) => {
+  let allLetter = document.querySelectorAll(
+    `.line-holder.row${num} > .word-holder`
+  );
+  lineComplete = false;
+  keyFunction(e, allLetter);
+  displayLetter(allLetter);
+  checkLetter(word);
+  if (lineComplete) {
+    word = "";
   }
-});
+};
+
+window.addEventListener(
+  "keyup",
+  (keyHandler = (e) => {
+    const isNumber = isFinite(e.key);
+    if (!isNumber) {
+      repeatFunction(e);
+    }
+  })
+);
 
 allKeys.forEach((key) => {
-  key.addEventListener("click", (e) => {
-    keyFunction(e);
-    checkLetter();
-    displayLetter();
-    
-  });
+  key.addEventListener(
+    "click",
+    (clickHandler = (e) => {
+      repeatFunction(e);
+    })
+  );
 });
