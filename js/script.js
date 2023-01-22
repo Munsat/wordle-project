21717b45bd81181dc264b75f8257a89d01c95e90
Munsat Rukaya <munsatrukaya@192-168-1-117.tpgi.com.au>
2023-01-23 01:25:22 +1100

    test 6-localstorage

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ createLetterBoxes();
 
 //Variables
 userScore = document.querySelector(".score span");
-if (!isNaN(parseInt(localStorage.score))){
+if (!isNaN(Number(localStorage.score))){
   userScore.textContent = localStorage.score;
 }else{
   localStorage.score = 0
@@ -71,8 +71,8 @@ const checkWinner = (allLetter) => {
     gameEndDisplay.style.opacity = 1;
     gameEndDisplay.style.visibility = "visible";
     wordDisplay.style.display = 'none';
-    if (!isNaN(parseInt(localStorage.score))){
-      localStorage.score = parseInt(localStorage.score) + 1
+    if (!isNaN(Number(localStorage.score))){
+      localStorage.score = Number(localStorage.score) + 1
       userScore.textContent = localStorage.score;
       gameEndDisplay.querySelector(".score-display span").textContent = localStorage.score;
     }
