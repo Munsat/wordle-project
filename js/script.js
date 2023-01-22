6e8f25c90aa4675a00e1affd9fe898bd5aa3d970
Munsat Rukaya <munsatrukaya@192-168-1-117.tpgi.com.au>
2023-01-23 00:31:41 +1100

    added flip transition, added key color change

--- js/script.js
+++ js/script.js
@@ -1,26 +1,7 @@
 const wordOfTheGame = validWords[Math.floor(Math.random() * validWords.length)];
 console.log(wordOfTheGame);
 
-//variables
-userScore = document.querySelector('.score')
-userScore.textContent = localStorage.score
-
-
-let word = "";
-let num = 0;
-let lineComplete = false;
-let greenLength = 0;
-const winnerTexts = ["GOOD JOB!", "WELL DONE", "WAY TO GO!", "WINNER!", "WOW!"];
-
-
-
-//DOM elements
-const allKeys = document.querySelectorAll(".key");
-const enterKey = document.querySelector("#enter");
-const winnerDisplay = document.querySelector(".winner-display");
-const nextGame = document.querySelector('.winner-display a')
-
-
+//Creates the display tiles
 const createLetterBoxes = () => {
   for (let i = 0; i < 6; i++) {
     const lineHolder = document.createElement("div");
@@ -34,6 +15,28 @@ const createLetterBoxes = () => {
   }
 };
 createLetterBoxes();
+
+//Variables
+userScore = document.querySelector(".score span");
+userScore.textContent = localStorage.score;
+
+let word = "";
+let rowNum = 0;
+let lineComplete = false;
+let greenLength = 0;
+const winnerTexts = ["GOOD JOB!", "WELL DONE", "WAY TO GO!", "WINNER!", "WOW!"];
+const green = 'rgb(96, 159, 141)'
+const orange = 'rgb(190, 101, 63)'
+const grey = 'rgb(120, 120, 120)'
+
+//DOM elements
+const allKeys = document.querySelectorAll(".key");
+const enterKey = document.querySelector("#enter");
+const gameEndDisplay = document.querySelector(".game-end-display");
+const nextGame = document.querySelector(".game-end-display a");
+const wordDisplay = document.querySelector('.word-display')
+const allRows = document.querySelectorAll('.line-holder')
+
 
 //Displays letter for each line
 const displayLetter = (allLetter) => {
@@ -57,63 +60,72 @@ const checkWinner = (allLetter) => {
     if (letter.style.backgroundColor === "rgb(96, 159, 141)") {
       greenLength++;
     }
-    if (greenLength === 5) {
-      winnerDisplay.querySelector("h1").textContent =
-        winnerTexts[Math.floor(Math.random() * winnerTexts.length)];
-        winnerDisplay.querySelector('a').textContent = 'next level'
-      winnerDisplay.style.opacity = 1;
-      winnerDisplay.style.visibility = 'visible'
-      if (localStorage.length){
-        currentScore = parseInt(localStorage.score)
-         currentScore+=1
-         localStorage.score = currentScore
-       }
-      userScore.textContent = localStorage.score
-      winnerDisplay.querySelector('.score-display span').textContent = localStorage.score
-    
-    }
-  });
+  })
+  if (greenLength === 5) {
+    gameEndDisplay.querySelector("h1").textContent = winnerTexts[Math.floor(Math.random() * winnerTexts.length)];
+    gameEndDisplay.querySelector("a").textContent = "Next Level";
+    gameEndDisplay.style.opacity = 1;
+    gameEndDisplay.style.visibility = "visible";
+    wordDisplay.style.display = 'none';
+    localStorage.score = parseInt(localStorage.score) + 1
+    userScore.textContent = localStorage.score;
+    gameEndDisplay.querySelector(".score-display span").textContent = localStorage.score;
+  };
   greenLength = 0;
 };
 
 //Checks if the user has lost the round
-const checkGameOver = (num) => {
-  if (num > 5) {
-    winnerDisplay.querySelector("h1").textContent = "GAME OVER";
-    winnerDisplay.querySelector('a').textContent = 'start over'
-    winnerDisplay.querySelector('.score-display span').textContent = localStorage.score
-    winnerDisplay.style.opacity = 1;
-    winnerDisplay.style.visibility = 'visible'
-    if (localStorage.length){
-      localStorage.score = 0
-    }
+const checkGameOver = (rowNum) => {
+  if (rowNum >= allRows.length-1) {
+    gameEndDisplay.querySelector("h1").textContent = "GAME OVER";
+    gameEndDisplay.querySelector("a").textContent = "Start Over";
+    gameEndDisplay.querySelector(".score-display span").textContent = localStorage.score;
+    gameEndDisplay.style.opacity = 1;
+    wordDisplay.style.display = 'block';
+    wordDisplay.querySelector('span').textContent = wordOfTheGame
+    gameEndDisplay.style.visibility = "visible";
+    localStorage.score = 0;
   }
-  
-  
 };
 
 //Displays the coloured tiles for each answer
 const displayAnswer = (allLetter) => {
   for (i = 0; i < word.length; i++) {
     if (wordOfTheGame[i] === word[i]) {
-      allLetter[i].style.backgroundColor = "rgb(96, 159, 141)";
+      allLetter[i].style.transform = 'rotateY(360deg)';
+      allLetter[i].style.backgroundColor = green;
+      allKeys.forEach((key) => {
+        if (key.textContent === word[i]){
+          key.style.backgroundColor = green
+        }
+      })
     } else if (wordOfTheGame.includes(word[i])) {
-      allLetter[i].style.backgroundColor = "rgb(190, 101, 63)";
+      allLetter[i].style.transform = 'rotateY(360deg)';
+      allLetter[i].style.backgroundColor = orange;
+      allKeys.forEach((key) => {
+        if (key.textContent === word[i] && key.style.backgroundColor !== green){
+          key.style.backgroundColor = orange
+        }
+      })
     } else {
-      allLetter[i].style.backgroundColor = "grey";
+      allLetter[i].style.backgroundColor = grey;
+      allLetter[i].style.transform = 'rotateY(360deg)';
+      allKeys.forEach((key) => {
+        if (key.textContent === word[i]){
+          key.style.backgroundColor = grey
+        }
+      })
     }
   }
   lineComplete = true;
-  num++;
   checkWinner(allLetter);
-  checkGameOver(num)
-  nextGame.addEventListener('click', ()=>{
-    location.reload()
-  })
-
+  checkGameOver(rowNum);
+  rowNum++;
+  nextGame.addEventListener("click", () => location.reload());
 };
 
 
+//Attaches functionality to keyboard keys and clicks
 const keyFunction = (e, allLetter) => {
   if (e.target.id === "backspace" || e.key === "Backspace") {
     word = word.slice(0, -1);
@@ -122,31 +134,33 @@ const keyFunction = (e, allLetter) => {
       e.preventDefault();
     } else {
       displayAnswer(allLetter);
+      enterKey.disabled = true;
     }
-  } else if (word.length<5 && ((e.key >= "a" && e.key <= "z") || (e.getModifierState("CapsLock") && (e.key >= "A" && e.key <= "Z" ) && e.key.length === 1))){
+  } else if (
+    word.length < 5 && ((e.key >= "a" && e.key <= "z") || (e.getModifierState("CapsLock") && (e.key >= "A" && e.key <= "Z") && e.key.length === 1))) {
     word += e.key.toUpperCase();
-  } else if (e.type === "click" && word.length<5) {
+  } else if (e.type === "click" && word.length < 5) {
     word += e.target.textContent;
   }
 };
 
+//Main function that is to be repeated for each row
 const repeatFunction = (e) => {
-  let allLetter = document.querySelectorAll(`.line-holder.row${num} > .word-holder`);
+  let allLetter = document.querySelectorAll(`.line-holder.row${rowNum} > .word-holder`);
   lineComplete = false;
-  keyFunction(e, allLetter)
-  if (word.length<=5){
-    
+  keyFunction(e, allLetter);
+
+  if (word.length <= 5) {
     displayLetter(allLetter);
+    if (lineComplete) {
+      word = "";
+    }
     checkLetter(word);
-  }
-  
-  
-  if (lineComplete) {
-    word = "";
   }
 };
 
-window.addEventListener("keyup", (keyHandler = (e) => {
+//EVENT LISTENER
+window.addEventListener("keyup",(keyHandler = (e) => {
     const isNumber = isFinite(e.key);
     if (!isNumber) {
       repeatFunction(e);
@@ -156,7 +170,7 @@ window.addEventListener("keyup", (keyHandler = (e) => {
 
 allKeys.forEach((key) => {
   key.addEventListener("click",(clickHandler = (e) => {
-      repeatFunction(e);
+    repeatFunction(e);
     })
   );
 });
